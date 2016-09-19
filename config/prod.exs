0d70094b5aa29194f28bcf67a8e8c5fbbd78244b
MainShayne233 <shaynetremblay@hotmail.com>
2016-09-19 01:15:54 -0400

    changes~

--- config/prod.exs
+++ config/prod.exs
@@ -9,11 +9,15 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or you later on).
 config :rocket_launches, RocketLaunches.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "rocketlaunches.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")#"hOKRGtZE0AM0osO+1yxBAax9kXdfTUSu/jEiv1By+W47Uq9mvyQSQjLmfzrYORqw"
 
 # Configure your database
 config :rocket_launches, RocketLaunches.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  database: "rocket_launches_prod",
-  pool_size: 20
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule RocketLaunches.UserSocket do
   # channel "room:*", RocketLaunches.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
